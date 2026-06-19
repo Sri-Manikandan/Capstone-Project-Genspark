b8aa4af88a22e856b10612140bf41ae39fe95ba7
Sri Manikandan <saisidharthan@synergeek.in>
2026-06-19 17:57:53 +0530

    pushing code to github

--- EMSAngular/src/environments/environment.ts
+++ EMSAngular/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:5222',
-  stripePublishableKey: 'pk_test_REPLACE_WITH_YOUR_KEY',
+  stripePublishableKey: 'pk_test_51Tk1KgDlDAgZgiPTi3aYubmttJEhLKybjjIiOjf9bPknQz70gArdOxFqQNZM2Z0tT90anDA3zw99DdVNCjf73Ul000LaAknaU3',
 };
